800b3208878ffd54e19904018415754dd3808914
MagicZL0 <tnwls9721@naver.com>
2019-10-10 15:45:48 +0900

    프로젝트 시작
    ZLSwitch 프로젝트 시작

---
+++ Assets/Scripts/Menu/GYSwitch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GYSwitch : MonoBehaviour
+{
+    [SerializeField] RectTransform handleRectTransform;
+
+    public MenuCtrl menuCtrl;
+
+    static bool isOn; //Turn : on, False: off
+
+    private void Start()
+    {
+        isOn = false;
+        handleRectTransform.anchoredPosition = new Vector2(-30, 0);
+    }
+    //스위치 0n/off 동작
+    public void OnClickSwitch()
+    {
+        if (isOn)
+        {
+            handleRectTransform.anchoredPosition = new Vector2(-30, 0);
+            isOn = false;
+        }
+        else
+        {
+            handleRectTransform.anchoredPosition = new Vector2(30, 0);
+            isOn = true;
+        }
+        menuCtrl.SwitchIsOn(isOn);
+    }
+}
---
+++ Assets/Scripts/Menu/MenuCtrl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuCtrl : MonoBehaviour
+{
+    public GameObject settingsPanel;
+
+    AudioSource audioSource;
+    private void Start()
+    {
+        settingsPanel.SetActive(false);
+        //audioSource.Play();
+        //audioSource.Stop();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OpenSettings()
+    {
+        //설정창 열기
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        //설정창 닫기
+        settingsPanel.SetActive(false);
+    }
+
+    //Switch를 위한 함수
+    public void SwitchIsOn(bool isOn)
+    {
+        if (isOn == true)
+            audioSource.Play();
+        else
+            audioSource.Pause();
+    }
+}
